04c4416ab122a5f33d8e22b3e6ef1c4351d16b3c
Abhishek Choudhary <76167003+AbhishekChoudhary23@users.noreply.github.com>
2024-06-19 20:44:48 +0530

    Update constants.js
    Updated Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AbhishekChoudhary23",
   resume:
-    "https://drive.google.com/file/d/1mpTKfz6wC05DT55CZJvNHHZawN0B8tFh/view?usp=sharing",
+    "https://drive.google.com/file/d/1J4_fFDTHB1MeL8AlYyDIcHXNJigJl16y/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhishek-choudhary-2232a8202/",
   geeks: "https://www.geeksforgeeks.org/user/abhishek_lbj_23/",
   leetcode: "https://leetcode.com/u/abhishek_lbj/",
